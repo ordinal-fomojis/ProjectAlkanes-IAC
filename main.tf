9ebb4eda92e1f6b93be6b4186b694b3d846bea41
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-06-29 17:01:07 +1000

    change to east us 2 (#3)

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ provider "azurerm" {
 
 locals {
   blobStorageAndContainer = "${azurerm_storage_account.storageAccount.primary_blob_endpoint}deploymentpackage"
-  location                = "East US"
+  location                = "East US 2"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -32,22 +32,19 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_service_plan" "servicePlan" {
-  name                   = "alkanes-serviceplan"
-  resource_group_name    = azurerm_resource_group.rg.name
-  location               = local.location
-  sku_name               = "B1"
-  os_type                = "Linux"
-  zone_balancing_enabled = false
+  name                = "alkanes-serviceplan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = local.location
+  sku_name            = "B1"
+  os_type             = "Linux"
 }
 
 resource "azurerm_storage_account" "storageAccount" {
-  name                            = "alkanesstorage"
-  resource_group_name             = azurerm_resource_group.rg.name
-  location                        = local.location
-  account_tier                    = "Standard"
-  account_replication_type        = "LRS"
-  allow_nested_items_to_be_public = false
-  shared_access_key_enabled       = false
+  name                     = "alkanesstorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = local.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "storageContainer" {
