ef28f2701721e64698e159044e82fda4018d0075
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-07-03 21:54:37 +1000

    add webapp (#13)
    * add webapp
    
    * format

--- main.tf
+++ main.tf
@@ -23,60 +23,59 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
-locals {
-  blobStorageAndContainer = "${azurerm_storage_account.storageAccount.primary_blob_endpoint}deploymentpackage"
-  location                = "East US 2"
+resource "random_pet" "random_id" {
+  separator = ""
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "alkanes-${var.env_name}"
-  location = local.location
+  location = "East US 2"
 }
 
-resource "azurerm_service_plan" "servicePlan" {
-  name                = "alkanes-serviceplan-${var.env_name}"
+resource "azurerm_service_plan" "service_plan" {
+  name                = "alkanes-serviceplan-${random_pet.random_id.id}-${var.env_name}"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = local.location
+  location            = azurerm_resource_group.rg.location
   sku_name            = "FC1"
   os_type             = "Linux"
 }
 
-resource "azurerm_storage_account" "storageAccount" {
-  name                     = "alkanesstorage${var.env_name}"
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "alkanesstorage${random_pet.random_id.id}${var.env_name}"
   resource_group_name      = azurerm_resource_group.rg.name
-  location                 = local.location
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "storageContainer" {
+resource "azurerm_storage_container" "storage_container" {
   name                  = "deploymentpackage"
-  storage_account_id    = azurerm_storage_account.storageAccount.id
+  storage_account_id    = azurerm_storage_account.storage_account.id
   container_access_type = "private"
 }
 
-resource "azurerm_log_analytics_workspace" "logAnalyticsWorkspace" {
-  name                = "alkanes-loganalytics-${var.env_name}"
-  location            = local.location
+resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
+  name                = "alkanes-loganalytics-${random_pet.random_id.id}-${var.env_name}"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   retention_in_days   = 30
 }
 
-resource "azurerm_application_insights" "appInsights" {
-  name                = "alkanes-appinsights-${var.env_name}"
-  location            = local.location
+resource "azurerm_application_insights" "app_insights" {
+  name                = "alkanes-appinsights-${random_pet.random_id.id}-${var.env_name}"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
-  workspace_id        = azurerm_log_analytics_workspace.logAnalyticsWorkspace.id
+  workspace_id        = azurerm_log_analytics_workspace.log_analytics_workspace.id
 }
 
-resource "azurerm_function_app_flex_consumption" "functionApps" {
-  name                        = "alkanes-functionapp-${var.env_name}"
+resource "azurerm_function_app_flex_consumption" "function_app" {
+  name                        = "alkanes-functionapp-${random_pet.random_id.id}-${var.env_name}"
   resource_group_name         = azurerm_resource_group.rg.name
-  location                    = local.location
-  service_plan_id             = azurerm_service_plan.servicePlan.id
+  location                    = azurerm_resource_group.rg.location
+  service_plan_id             = azurerm_service_plan.service_plan.id
   storage_container_type      = "blobContainer"
-  storage_container_endpoint  = local.blobStorageAndContainer
+  storage_container_endpoint  = "${azurerm_storage_account.storage_account.primary_blob_endpoint}${azurerm_storage_container.storage_container.name}"
   storage_authentication_type = "SystemAssignedIdentity"
   runtime_name                = "node"
   runtime_version             = "22"
@@ -85,17 +84,41 @@ resource "azurerm_function_app_flex_consumption" "functionApps" {
     type = "SystemAssigned"
   }
   site_config {
-    application_insights_connection_string = azurerm_application_insights.appInsights.connection_string
+    application_insights_connection_string = azurerm_application_insights.app_insights.connection_string
   }
   app_settings = {
     "AzureWebJobsStorage"              = "" //workaround until https://github.com/hashicorp/terraform-provider-azurerm/pull/29099 gets released
-    "AzureWebJobsStorage__accountName" = azurerm_storage_account.storageAccount.name
-    "DOTENV_PRIVATE_KEY_PRODUCTION"    = "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.keyVault.name};SecretName=DotenvPrivateKey)"
+    "AzureWebJobsStorage__accountName" = azurerm_storage_account.storage_account.name
+    "DOTENV_PRIVATE_KEY_PRODUCTION"    = "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.key_vault.name};SecretName=DotenvPrivateKey)"
   }
 }
 
-resource "azurerm_key_vault" "keyVault" {
-  name                        = "alkanes-kv-${var.env_name}"
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "alkanes-webapp-${random_pet.random_id.id}-${var.env_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.service_plan.id
+  https_only          = true
+
+  app_settings = {
+    "DOTENV_PRIVATE_KEY_PRODUCTION" = "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.key_vault.name};SecretName=DotenvPrivateKey)"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    health_check_path                 = "/health"
+    health_check_eviction_time_in_min = 3
+    application_stack {
+      node_version = "22-lts"
+    }
+  }
+}
+
+resource "azurerm_key_vault" "key_vault" {
+  name                        = "alkanes-kv-${random_pet.random_id.id}-${var.env_name}"
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
@@ -106,15 +129,22 @@ resource "azurerm_key_vault" "keyVault" {
 }
 
 resource "azurerm_role_assignment" "storage_roleassignment" {
-  scope                = azurerm_storage_account.storageAccount.id
+  scope                = azurerm_storage_account.storage_account.id
   role_definition_name = "Storage Blob Data Owner"
-  principal_id         = azurerm_function_app_flex_consumption.functionApps.identity.0.principal_id
+  principal_id         = azurerm_function_app_flex_consumption.function_app.identity.0.principal_id
   principal_type       = "ServicePrincipal"
 }
 
-resource "azurerm_role_assignment" "keyvault_roleassignment" {
-  scope                = azurerm_key_vault.keyVault.id
+resource "azurerm_role_assignment" "keyvault_function_roleassignment" {
+  scope                = azurerm_key_vault.key_vault.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_function_app_flex_consumption.functionApps.identity.0.principal_id
+  principal_id         = azurerm_function_app_flex_consumption.function_app.identity.0.principal_id
+  principal_type       = "ServicePrincipal"
+}
+
+resource "azurerm_role_assignment" "keyvault_webapp_roleassignment" {
+  scope                = azurerm_key_vault.key_vault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_linux_web_app.webapp.identity.0.principal_id
   principal_type       = "ServicePrincipal"
 }
