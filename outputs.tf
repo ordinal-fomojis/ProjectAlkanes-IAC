9b7b268242981cd1b4864ba6ebfacf2f3a2f774b
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-07-12 15:36:37 +1000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@ output "function_app_name" {
   value = azurerm_linux_function_app.function_app.name
 }
 
-output "webapp_name" {
-  value = azurerm_linux_web_app.webapp.name
-}
+# output "webapp_name" {
+#   value = azurerm_linux_web_app.webapp.name
+# }
 
 # output "mock_webapp_name" {
 #   value = azurerm_linux_web_app.mock_webapp.name
