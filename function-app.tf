294058cb6a76727cc9a11343034656c01812e0ed
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-09-01 21:29:33 +1000

    separate function into separate service plan (#57)

---
+++ function-app.tf
@@ -0,0 +1,88 @@
+resource "azurerm_service_plan" "function_app_service_plan" {
+  name                = "shovel-serviceplan${local.postfix}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku_name            = "B1"
+  os_type             = "Linux"
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "shovelstorage${var.id}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "deploymentpackage"
+  storage_account_id    = azurerm_storage_account.storage_account.id
+  container_access_type = "private"
+}
+
+resource "azurerm_linux_function_app" "function_app" {
+  name                = "shovel-functionapp${local.postfix}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  service_plan_id = azurerm_service_plan.function_app_service_plan.id
+
+  storage_account_name       = azurerm_storage_account.storage_account.name
+  storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
+  https_only                 = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    application_insights_connection_string = azurerm_application_insights.app_insights.connection_string
+    always_on                              = true
+
+    application_stack {
+      node_version = "22"
+    }
+    cors {
+      allowed_origins = ["https://portal.azure.com"]
+    }
+  }
+
+  app_settings = {
+    "NODE_ENV"                = "production"
+    "APP_ENV"                 = "prod"
+    "DOTENV_PRIVATE_KEY_PROD" = "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.key_vault.name};SecretName=DotenvPrivateKeyProd)"
+    "DOTENV_PATH"             = "env/.env.prod"
+  }
+}
+
+resource "azurerm_linux_function_app_slot" "function_app_nonprod_slot" {
+  name = "nonprod"
+
+  function_app_id            = azurerm_linux_function_app.function_app.id
+  storage_account_name       = azurerm_storage_account.storage_account.name
+  storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
+  https_only                 = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    application_insights_connection_string = azurerm_application_insights.app_insights.connection_string
+    always_on                              = false
+
+    application_stack {
+      node_version = "22"
+    }
+    cors {
+      allowed_origins = ["https://portal.azure.com"]
+    }
+  }
+
+  app_settings = {
+    "NODE_ENV"                   = "production"
+    "APP_ENV"                    = "nonprod"
+    "DOTENV_PRIVATE_KEY_NONPROD" = "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.key_vault.name};SecretName=DotenvPrivateKeyNonProd)"
+    "DOTENV_PATH"                = "env/.env.nonprod"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "function_app_name" {
   value = azurerm_linux_function_app.function_app.name
 }
 
-output "webapp_name" {
+output "webapp_prod_name" {
   value = azurerm_linux_web_app.webapp.name
+}
+
+output "webapp_nonprod_name" {
+  value = azurerm_linux_web_app.webapp_nonprod.name
 }
